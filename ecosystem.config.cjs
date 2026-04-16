52a3e54e0b22cbb17ca4cc4a3fc70b656ac40bf5
Grumble <hujik1234@gmail.com>
2026-04-16 22:10:21 +0900

    remove: webhook 서버 기능 제거

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,15 +2,9 @@ module.exports = {
   apps: [
     {
       name: "serverDiscordBot",
-      script: "src/bot/index.js",
+      script: "./src/bot/index.js",
       watch: true, // 코드 변경 시 자동 재시작
       autorestart: true, // 크래시 시 자동 재시작
-    },
-    {
-      name: "webhook-server",
-      script: "src/bot/webhook.js",
-      watch: false,
-      autorestart: true,
     },
   ],
 };
